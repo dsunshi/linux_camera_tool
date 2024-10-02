7580478ea06c952607d4c2831495f2d51bfd58bd
D. Sunshine <david@sunshines.org>
2024-10-02 02:16:47 +0000

    Building with OpenCV 3.2

--- configure.sh
+++ configure.sh
@@ -1,11 +1,12 @@
 #/bin/sh
 sudo apt-get update
-sudo apt-get -y install build-essential cmake pkg-config    #for g++ build tool
-sudo apt-get -y install clang                               #for clang, optional
-sudo apt-get -y install libgtk-3-dev                        #for gtk3 GUI
-sudo apt-get -y install libomp-dev                          #for openmp
-sudo apt-get -y install v4l-utils libv4l-dev                #for v4l2
-sudo apt-get -y install libudev-dev                         #for udev
-sudo apt-get -y install libopencv-dev                       #for opencv dependencies
-sudo apt-get -y install libjson-c-dev                       #for json parser
-sudo apt-get -y install libpng-dev                          #for capturing png
+sudo apt-get -y install build-essential cmake pkg-config    # for g++ build tool
+sudo apt-get -y install clang                               # for clang, optional
+sudo apt-get -y install libgtk-3-dev                        # for gtk3 GUI
+sudo apt-get -y install libomp-dev                          # for openmp
+sudo apt-get -y install v4l-utils libv4l-dev                # for v4l2
+sudo apt-get -y install libudev-dev                         # for udev
+#sudo apt-get -y install libopencv-dev                      # for opencv dependencies
+./OpenCV3.2withContrib.sh                                   # OpenCV 3.2 with Contrib install script [CUDA supported]
+sudo apt-get -y install libjson-c-dev                       # for json parser
+sudo apt-get -y install libpng-dev                          # for capturing png
